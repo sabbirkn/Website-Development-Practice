06c1a5af571a7846cb3efbe8e096b6c877dbc573
sabbirkn <sabbirkhan92860@gmail.com>
2022-01-13 12:57:30 +0600

    I changed JS

--- HTML/Js/script.js
+++ HTML/Js/script.js
@@ -1,5 +1,5 @@
 function changedtext () {
-    document.getElementById('originaltxt') .innerHTML = 'This is our changed text';
+    document.getElementById('originaltxt') .innerHTML = 'This is our changed text two';
     document.getElementById('originaltxt') .style.color = 'red';
     document.getElementById('originaltxt') .style.fontSize = '30px';
     document.getElementById('originaltxt') .style.backgroundColor = 'yellow';
