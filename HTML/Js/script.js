bb63e816906071e7709315c3a53f22008b806667
sabbirkn <sabbirkhan92860@gmail.com>
2022-01-13 12:12:57 +0600

    File Upload

---
+++ HTML/Js/script.js
@@ -0,0 +1,7 @@
+function changedtext () {
+    document.getElementById('originaltxt') .innerHTML = 'This is our changed text';
+    document.getElementById('originaltxt') .style.color = 'red';
+    document.getElementById('originaltxt') .style.fontSize = '30px';
+    document.getElementById('originaltxt') .style.backgroundColor = 'yellow';
+}
+
