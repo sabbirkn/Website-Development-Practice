95f8429fc1b83667cc65969c3c7f6fef1f1b1e90
sabbirkn <sabbirkhan92860@gmail.com>
2022-01-25 22:55:39 +0600

    Project 1 Completed

---
+++ Project-1/js/script.js
@@ -0,0 +1,5 @@
+function ourclick()  {
+document.getElementById('ctahead').innerHTML="This is our changed heading";
+document.getElementById('ctahead').style.color="rgb(210, 252, 252)";
+document.getElementById('ctahead').style.fontSize="50px";
+}
